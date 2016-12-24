d70197add0070cc1d4ca04b977ac16a545188fe6
Panchami K.S <panchami.ks1@gmail.com>
2016-12-24 22:40:27 +0530

    Answer evaluation initial implementation for all the three cases.

--- CustomMethods.py
+++ CustomMethods.py
@@ -104,30 +104,70 @@ def saveTrainingData(images, clusters):
 
 def diagramEvaluation():
         images = []
-        images.append(processImage('images/diag_err_excess.bmp'))
+        images.append(processImage('images/diag.bmp'))
         with open('trained_data.pkl', 'rb') as f:
             data = dill.load(f)
 
         points = generateAllContourPointsForClustering(images)
 
         clusters = kmeansClassification(data.clusters, points)
-        score = evaluateAnswerPoints(points, clusters)
+
+        evaluateAnswerPoints(points, clusters)
+
         showClusters(clusters)
 
-        print "Score : " , score
-def findCluster(p,clusters):
-    for i, cluster in enumerate(clusters):
-         if p in cluster.points:
-              return cluster
+
 
 def evaluateAnswerPoints(points, clusters):
+    # Checking whether the answer diagram and trained data has equal no of text blocks/regions or not.
+    check = len(points) - len(clusters)
+
+    if check == 0:
+        evaluateAnswerPointsEqual(points, clusters)
+    elif check < 0:
+        evaluateAnswerPointsLesser(points, clusters)
+    else:
+        evaluateAnswerPointsGreater(points, clusters)
+
+# To handle scenario with question diagrams having equal no of text regions/blocks.
+def evaluateAnswerPointsEqual(points, clusters):
+    print "Handling equal point - cluster scenario."
     count = 0
     for p in points:
         cluster = findCluster(p, clusters)
         if cluster.points[1].text == p.text:
             count += 1
-            # print cluster.points[0].text, p.text
-    return count
+    print "Score : ", count
+    pass
+
+
+# To handle error scenario with question diagrams having lesser text regions/blocks.
+def evaluateAnswerPointsLesser(points, clusters):
+    print "Handling lesser point than cluster scenario."
+    count = 0
+    for p in points:
+        cluster = findCluster(p, clusters)
+        if cluster.points[1].text == p.text:
+            count += 1
+    print "Score : ", count
+    pass
+
+# To handle error scenario with question diagrams having more text regions/blocks.
+def evaluateAnswerPointsGreater(points, clusters):
+    print "Handling greater point than cluster scenario."
+    count = 0
+    for p in points:
+        cluster = findCluster(p, clusters)
+        if cluster.points[1].text == p.text:
+            count += 1
+    print "Score : ", count
+    pass
+
+
+def findCluster(p,clusters):
+    for i, cluster in enumerate(clusters):
+         if p in cluster.points:
+              return cluster
 
 
 
